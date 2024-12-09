ac3dff534121fa66a700949472a904b90d73de3a
kwontaein <zodls1128@gmail.com>
2024-12-10 08:49:59 +0900

    AdminService 세팅

--- src/main/java/com/example/daon/controller/AdminController.java
+++ src/main/java/com/example/daon/controller/AdminController.java
@@ -1,4 +1,15 @@
 package com.example.daon.controller;
-/**고객관리*/
+
+import com.example.daon.service.AdminService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 회사 사원관리
+ */
+@RestController
+@RequiredArgsConstructor
 public class AdminController {
+    private final AdminService adminService;
+    
 }
--- src/main/java/com/example/daon/controller/InventoryController.java
+++ src/main/java/com/example/daon/controller/InventoryController.java
@@ -1,4 +1,8 @@
 package com.example.daon.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
 /**품목재고*/
+@RestController
 public class InventoryController {
 }
