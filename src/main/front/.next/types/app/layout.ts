7e99ef26f606528a67bf5b3ee9e27b26b2e77ecb
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-01-03 15:40:58 +0900

    chore: Add header-mobile version

--- src/main/front/.next/types/app/layout.ts
+++ src/main/front/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: C:\Users\SeungJae\Desktop\project\daon\src\main\front\app\layout.js
+// File: C:\Users\SeungJae\Desktop\project\daon\src\main\front\app\layout.tsx
 import * as entry from '../../../app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
