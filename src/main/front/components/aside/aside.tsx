217cbf981dce386b73817ea383a33d4f37862a72
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-01-03 15:40:58 +0900

    chore: Add header-mobile version

---
+++ src/main/front/components/aside/aside.tsx
@@ -0,0 +1,20 @@
+import { AsideOptions } from "@/app/actions/asideOptions";
+import Link from "next/link";
+import './aside.scss'
+
+export default function MainAside({nav}){
+    
+    return(
+        <aside className="aside-container">
+            <div className="aside-header">
+                {AsideOptions[nav].asideTitle}
+            </div>
+            {AsideOptions[nav].asideItems.map((item,idx)=>(
+                <li key={item.name}>
+                    <b>ㆍ</b><Link href={``}>{item.name}</Link>
+                </li>
+            ))}
+        </aside>
+    )
+}
+
