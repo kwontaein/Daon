e585cfd66ed2f2ef5e27725b9623d3db9149a2c3
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-01-10 00:31:32 +0900

    chore: Complete mobile navigation

---
+++ src/main/front/components/aside/mobile/_mobile-aside.tsx
@@ -0,0 +1,24 @@
+import { AsideOptions } from "@/constants/asideOptions";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
+import './_mobile-aside.scss';
+
+export default function MobileAsideBar({nav}){
+
+    return(
+        <section className={'aside-mobile-wrapper'}>
+        <div className={'aside-mobile-title'}>
+            <b>ㆍ</b> {AsideOptions[nav|| 'schedule'].asideTitle}
+        </div>
+        <ul className={'aside-mobile-ul'}>
+           {AsideOptions[nav || 'schedule'].asideItems.map((item)=>(
+            <li key={item.link}>
+               <Link href={`/main/${nav}/${item.link}?toggle=true`}>{item.name}</Link>
+               <FontAwesomeIcon icon={faArrowRight} className='aside-arrow'/>                    </li>
+            ))}
+        </ul>
+    </section>
+    )
+}
