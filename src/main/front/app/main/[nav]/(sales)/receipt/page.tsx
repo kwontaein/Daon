518ae058dee9f844d893d28f0bafdf44291fe74f
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-01-22 21:05:50 +0900

    feat: Add fullCalendar + redux - fullcalendar를 스타일링 - redux를 통한 date 상태관리 - fullcalendar api를 활용한 날짜 조작 및 선택

--- src/main/front/app/main/[nav]/(sales)/receipt/page.tsx
+++ src/main/front/app/main/[nav]/(sales)/receipt/page.tsx
@@ -1,16 +1,24 @@
 import ReceiptSearch from "@/components/main-view/sales/receipt/search";
 import RegisterButton from "@/components/main-view/sales/receipt/button";
 import ReceiptTableHeader from "@/components/main-view/sales/receipt/table/table-header";
+import Calendar from "@/components/main-view/sales/receipt/aside/calendar";
+
+import '@/styles/_global.scss';
 
 export default function ReceiptPage() {
     return (
-        <section>
-            <ReceiptSearch/>
-            <RegisterButton/>
-            
-            <ReceiptTableHeader>
-                <tbody></tbody>
-            </ReceiptTableHeader>
-        </section>
+        <div className="flex-row">
+            <section className="full-screen">
+                <ReceiptSearch/>
+                <RegisterButton/>
+                
+                <ReceiptTableHeader>
+                    <tbody></tbody>
+                </ReceiptTableHeader>
+            </section>
+            <section style={{marginInline:'1rem'}}>
+                <Calendar/>
+            </section>
+        </div>
     );
 }
