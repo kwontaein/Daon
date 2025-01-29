e5283c86145179330c1f69f04a95be4b719b9339
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-01-29 23:08:47 +0900

    feat: Progress customer page

---
+++ src/main/front/app/main/[nav]/(customer)/customer/page.tsx
@@ -0,0 +1,9 @@
+import CustomerSearch from "@/components/main-view/customer/search";
+
+export default function CustomerPage(){
+    return(
+        <section>
+            <CustomerSearch/>
+        </section>
+    )
+}
