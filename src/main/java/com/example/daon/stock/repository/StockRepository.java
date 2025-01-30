45e3a1851a689c72b671aed57fb3e1cbb240bcaf
kwontaein <zodls1128@gmail.com>
2025-01-30 17:46:01 +0900

    24_01_28

--- src/main/java/com/example/daon/stock/repository/StockRepository.java
+++ src/main/java/com/example/daon/stock/repository/StockRepository.java
@@ -1,4 +1,12 @@
 package com.example.daon.stock.repository;
 
-public interface StockRepository {
+import com.example.daon.stock.model.StockEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface StockRepository extends JpaRepository<StockEntity, UUID>, JpaSpecificationExecutor<StockEntity> {
 }
