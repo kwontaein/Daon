fe82685c831dab57daad3ac38666dbf4cde9e352
kwontaein <zodls1128@gmail.com>
2025-02-03 20:21:10 +0900

    feat : new api create

---
+++ src/main/java/com/example/daon/customer/repository/CustomerBillRepository.java
@@ -0,0 +1,14 @@
+package com.example.daon.customer.repository;
+
+import com.example.daon.customer.model.CustomerBillEntity;
+import com.example.daon.customer.model.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerBillRepository extends JpaRepository<CustomerBillEntity, UUID>, JpaSpecificationExecutor<CustomerBillEntity> {
+    Optional<CustomerBillEntity> findByCustomer(@Param("customer") CustomerEntity customer);
+}
