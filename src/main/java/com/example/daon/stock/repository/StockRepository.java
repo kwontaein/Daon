e129d0a25136bd49a66546d643be98c30fde99ef
kwontaein <zodls1128@gmail.com>
2025-02-04 19:28:06 +0900

    feat : new api create

--- src/main/java/com/example/daon/stock/repository/StockRepository.java
+++ src/main/java/com/example/daon/stock/repository/StockRepository.java
@@ -3,10 +3,13 @@ package com.example.daon.stock.repository;
 import com.example.daon.stock.model.StockEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface StockRepository extends JpaRepository<StockEntity, UUID>, JpaSpecificationExecutor<StockEntity> {
+    Optional<StockEntity> findByName(@Param("name") String name);
 }
