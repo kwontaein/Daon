7d2ce06b1c6572e255d283b143ba46f1a4b8191a
kwontaein <zodls1128@gmail.com>
2025-02-07 17:47:24 +0900

    refactor : getCustomer api bug fix and cors problem solved

--- src/main/java/com/example/daon/config/WebMvcConfig.java
+++ src/main/java/com/example/daon/config/WebMvcConfig.java
@@ -1,15 +1,12 @@
 package com.example.daon.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
+@Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
-    /* @Override
-     public void addCorsMappings(CorsRegistry registry) {//http://localhost:3000
-         registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET");
-     }*/
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
