832f74993ffd8f721c3c4899bb72ec4c1f4ae5fb
kwontaein <zodls1128@gmail.com>
2025-02-12 01:00:27 +0900

    feat : create update api

--- src/main/java/com/example/daon/admin/repository/UserRepository.java
+++ src/main/java/com/example/daon/admin/repository/UserRepository.java
@@ -2,10 +2,12 @@ package com.example.daon.admin.repository;
 
 import com.example.daon.admin.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
-import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findById(String id);
+
+    void save(UserEntity entity, PasswordEncoder passwordEncoder);
 }
