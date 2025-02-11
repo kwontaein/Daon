eef270fc0fab68ce4cff721f98500a9ea22be402
kwontaein <zodls1128@gmail.com>
2025-02-11 14:59:55 +0900

    feat : create update api

--- src/main/java/com/example/daon/customer/dto/request/CustomerRequest.java
+++ src/main/java/com/example/daon/customer/dto/request/CustomerRequest.java
@@ -1,6 +1,8 @@
 package com.example.daon.customer.dto.request;
 
 import com.example.daon.admin.model.UserEntity;
+import com.example.daon.customer.model.CustomerCate;
+import com.example.daon.customer.model.CustomerCateEntity;
 import com.example.daon.customer.model.CustomerEntity;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -15,33 +17,63 @@ public class CustomerRequest {
     private UUID customerId;
     //상호명
     private String customerName;
+    private String billName;
     //고객정보
     private String contactInfo;
-
-    private String category;
-
     private String phoneNumber;
-
     private String fax;
-
     private String userId;
 
     //------------
     private UUID cateId;
-
     private String ceo;
+    private String ceoNum;
     private String searchTarget;
+    private String companyNum;
+    private String businessType;
+    private String contents;
+    private String customerRp;
+    private String customerRpCall;
+    private String bankName;
+    private String bankNum;
+    private String bankOwner;
+    private String handlingItem;
+    private String memo;
+    private CustomerCate category;
+    private String zipCode;
+    private String address1;
+    private String address2;
+    private String etc;
+    private UserEntity user;
 
 
-    public CustomerEntity toEntity(UserEntity user) {
+    public CustomerEntity toEntity(UserEntity user, CustomerCateEntity customerCate) {
         return CustomerEntity
                 .builder()
-                .customerId(customerId)
+                .user(user)
+                .ceo(ceo)
+                .ceoNum(ceoNum)
+                .companyNum(customerName)
+                .businessType(businessType)
+                .contents(contents)
+                .customerRp(customerRp)
+                .customerRpCall(customerRpCall)
+                .bankName(bankName)
+                .bankNum(bankNum)
+                .bankOwner(bankOwner)
+                .handlingItem(handlingItem)
+                .memo(memo)
+                .category(category)
+                .zipCode(zipCode)
+                .address1(address1)
+                .address2(address2)
+                .etc(etc)
                 .customerName(customerName)
+                .billName(billName)
                 .contactInfo(contactInfo)
                 .phoneNumber(phoneNumber)
                 .fax(fax)
-                .user(user)
+                .customerCateId(customerCate)
                 .build();
     }
 }
