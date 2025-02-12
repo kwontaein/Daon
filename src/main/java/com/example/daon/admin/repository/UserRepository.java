f6a558c57c4f56f3844d4545ce4bf7938f99fc5e
kwontaein <zodls1128@gmail.com>
2025-02-12 14:59:17 +0900

    refactor : 잘못된 repository 연결 수정 및 dto 수정

--- src/main/java/com/example/daon/admin/repository/UserRepository.java
+++ src/main/java/com/example/daon/admin/repository/UserRepository.java
@@ -2,12 +2,9 @@ package com.example.daon.admin.repository;
 
 import com.example.daon.admin.model.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<UserEntity, String> {
     Optional<UserEntity> findById(String id);
-
-    void save(UserEntity entity, PasswordEncoder passwordEncoder);
 }
