ad3d597bd302ac3a9f6463d8e9e5a3af4525bfe8
kwontaein <zodls1128@gmail.com>
2025-02-20 13:22:04 +0900

    feat : StockCateRequest 생성

--- src/main/java/com/example/daon/stock/dto/request/StockCateRequest.java
+++ src/main/java/com/example/daon/stock/dto/request/StockCateRequest.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 
 @Data
 public class StockCateRequest {
+
     private UUID stockCateId; // 아이디 - uuid
     private String stockCateName; // 분류 명
     private String cateKey; // 분류 키
