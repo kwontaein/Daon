1bb57f90e17927775960ec196893fbb41d9e0a3c
kwontaein <zodls1128@gmail.com>
2025-02-20 15:40:56 +0900

    feat : 수정

--- src/main/java/com/example/daon/stock/dto/request/StockCateRequest.java
+++ src/main/java/com/example/daon/stock/dto/request/StockCateRequest.java
@@ -10,12 +10,10 @@ public class StockCateRequest {
 
     private UUID stockCateId; // 아이디 - uuid
     private String stockCateName; // 분류 명
-    private String cateKey; // 분류 키
 
     public StockCate toEntity() {
         return StockCate
                 .builder()
-                .cateKey(cateKey)
                 .stockCateName(stockCateName)
                 .build();
     }
