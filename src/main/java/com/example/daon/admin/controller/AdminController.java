56605aada45dadc9f75b1d5eeccd92eb09d1096b
kwontaein <zodls1128@gmail.com>
2025-02-27 06:58:34 +0900

    feat : 업무관리 api 생성

--- src/main/java/com/example/daon/admin/controller/AdminController.java
+++ src/main/java/com/example/daon/admin/controller/AdminController.java
@@ -38,8 +38,8 @@ public class AdminController {
     }
 
     //사원정보 crud
-    @PostMapping("api/createEmployee")
-    public void CreateEmployee(@RequestBody UserRequest userRequest) {
+    @PostMapping("api/saveEmployee")
+    public void saveEmployee(@RequestBody UserRequest userRequest) {
         adminService.CreateEmployee(userRequest);
     }
 
@@ -69,8 +69,8 @@ public class AdminController {
         return adminService.getDept();
     }
 
-    @PostMapping("api/createDept")
-    public void CreateDept(@RequestBody DeptRequest deptRequest) {
+    @PostMapping("api/saveDept")
+    public void saveDept(@RequestBody DeptRequest deptRequest) {
         adminService.CreateDept(deptRequest);
     }
 
