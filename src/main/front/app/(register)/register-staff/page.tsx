1af924c1fc2c081bf7ae7916d1e4ba4dec6212da
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-02-28 16:22:00 +0900

    feat: Make Company Form Component

--- src/main/front/app/(register)/register-staff/page.tsx
+++ src/main/front/app/(register)/register-staff/page.tsx
@@ -2,8 +2,6 @@ import StaffForm from "@/components/main/staff/staff/form/staff-form";
 
 export default function RegisterStaff(){
     return(
-        <>
             <StaffForm/>
-        </>
     )
 }
