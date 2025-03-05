9743266e4a5d8362fc53bdef3d85b800dc763e88
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-05 13:07:38 +0900

    refactor: wrapping useCallback to pageByState

--- src/main/front/store/slice/task-search.tsx
+++ src/main/front/store/slice/task-search.tsx
@@ -7,27 +7,25 @@ type TaskSearchConditionKeys = keyof TaskSearchCondition;
 
 export interface TaskSearchCondition {
     customer:string,
-    taskType: TaskType|'none', //구분
-    customerCate:string, //거래처구분
-    assignedUser:string, //담당자
+    taskType: TaskType|null, //구분
+    customerCate:string|null, //거래처구분
+    assignedUser:string|null, //담당자
 } 
 
 export interface TaskSearch{
     postSearchInfo: TaskSearchCondition,
     isSearch: boolean
-    allView :boolean,
 }
 
 
 const initialState:TaskSearch={
     postSearchInfo:{
-        customer:'none',
-        taskType: 'none', //구분
-        customerCate:'none', //거래처구분
-        assignedUser:'', //담당자
+        customer: '',
+        taskType: null, //구분
+        customerCate: null, //거래처구분
+        assignedUser:null, //담당자
     },
     isSearch:false,
-    allView :false,
 }
 
 const TaskSearch = createSlice({
@@ -35,7 +33,10 @@ const TaskSearch = createSlice({
     initialState,
     reducers:{
         updateTaskSearchQuery: (state, action: PayloadAction<Partial<Record<TaskSearchConditionKeys, TaskSearchCondition[TaskSearchConditionKeys]>>>)=>{
-            Object.assign(state.postSearchInfo, action.payload);
+            for(let [key,value] of Object.entries(action.payload)){
+                if(value ==='none') value = null
+                state.postSearchInfo[key] = value;
+            }
         },
         updateTaskSearchInput: (state, action: PayloadAction<string>)=>{
             state.postSearchInfo.assignedUser = action.payload
@@ -46,13 +47,10 @@ const TaskSearch = createSlice({
         ResetTaskSearchQuery: (state)=>{
             state.postSearchInfo = initialState.postSearchInfo;
         },
-        RequestAllTaskData: (state, action:PayloadAction<boolean>)=>{
-            state.allView = action.payload;
-        },
     }
 })
 
-export const {updateTaskSearchQuery, updateTaskSearchInput, RequestTaskData, ResetTaskSearchQuery,RequestAllTaskData} = TaskSearch.actions;
+export const {updateTaskSearchQuery, updateTaskSearchInput, RequestTaskData, ResetTaskSearchQuery} = TaskSearch.actions;
 
 
 export default TaskSearch.reducer;
