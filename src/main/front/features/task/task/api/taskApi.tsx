0b21f213d04d01bfc3404793e27d9f680f70b869
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-08 02:46:03 +0900

    Fixed: delete eventListener when occur duplicate event

---
+++ src/main/front/features/task/task/api/taskApi.tsx
@@ -0,0 +1,35 @@
+import { SaveTask, TaskSearchCondition } from "@/model/types/task/task/type";
+import { revalidatePath } from "next/cache";
+
+export const fetchSearchTask = async (searchCondition:TaskSearchCondition)=>{
+    try {
+        const response = await fetch("http://localhost:8080/api/getTaskByOption", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(searchCondition),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const text = await response.text();
+        return text ? JSON.parse(text) : [];
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export const saveTask = async (task:SaveTask)=>{
+    try {
+        const response = await fetch("http://localhost:8080/api/saveTask", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(task),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+        return response.status;
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
