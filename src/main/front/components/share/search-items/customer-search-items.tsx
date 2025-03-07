a5f44255cbcf5b7e70f06d9b4bdf09005a87dc3e
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-07 15:31:27 +0900

    feat: before rebase commit

--- src/main/front/components/share/search-items/customer-search-items.tsx
+++ src/main/front/components/share/search-items/customer-search-items.tsx
@@ -1,15 +1,25 @@
 'use client'
-import { ResponseCustomer } from '@/model/types/customer/customer/type';
 import '@/styles/table-style/search-result.scss';
+import '@/styles/_global.scss'
+import { ResponseCustomer } from '@/model/types/customer/customer/type';
 import { useMemo, useState } from 'react';
+import Pagination from '../pagination';
+import { CustomerCategoryMap } from '@/model/constants/customer/customer-data';
 export default function CustomerSearchItems({customers, page, pageLength} : {
-    customers: ResponseCustomer,
+    customers: ResponseCustomer[],
     page: number,
     pageLength: number
 }) {
 
-
+    const selectValue = (value: string) => {
+        if (window.opener) {
+          window.opener.postMessage(value, "*");
+          window.close();
+        }
+    };
+      
     return(
+        <section>
         <table className='search-result-table'>
             <thead>
                 <tr>
@@ -21,8 +31,22 @@ export default function CustomerSearchItems({customers, page, pageLength} : {
                 </tr>
             </thead>
             <tbody>
-
+                {customers.map((customer) => (
+                    <tr key={customer.customerId} onClick={selectValue.bind(null,customer)}>
+                        <td className='left-align'>{customer.customerName}</td>
+                        <td>{CustomerCategoryMap[customer.category]}</td>
+                        <td>{customer.phoneNumber}</td>
+                        <td>{customer.fax}</td>
+                        <td>{customer.etc}</td>
+                    </tr>
+                ))}
             </tbody>
         </table>
+        <Pagination
+                    totalItems={pageLength}
+                    itemCountPerPage={20} 
+                    pageCount={5} 
+                    currentPage={Number(page)}/>
+        </section>
     )
 }
