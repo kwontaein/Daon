da656581a5aae0467afe013aee5980e1f75c093e
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-11 16:58:03 +0900

    feat: Set revalidTag to task

--- src/main/front/app/main/[nav]/(task)/task/page.tsx
+++ src/main/front/app/main/[nav]/(task)/task/page.tsx
@@ -1,19 +1,20 @@
 import TaskSearch from "@/components/main/task/task/search";
 import TaskSearchResult from "@/components/main/task/task/search-result";
 
-import {CustomerAffiliation} from "@/model/types/customer/affiliation/type";
-import {PageByProps} from "@/model/types/share/type";
-import {ResponseTask} from "@/model/types/task/task/type";
+import { CustomerAffiliation } from "@/model/types/customer/affiliation/type";
+import { PageByProps } from "@/model/types/share/type";
+import { ResponseEmployee } from "@/model/types/staff/employee/type";
+import { ResponseTask } from "@/model/types/task/task/type";
 
-export default async function TaskPage({searchParams}: PageByProps) {
+export default async function TaskPage({searchParams}:PageByProps){
     const page = (await searchParams).page || 1;
 
     const controller = new AbortController();
     const signal = controller.signal;//작업 취소 컨트롤
-    const timeoutId = setTimeout(() => controller.abort(), 10000)
+    const timeoutId = setTimeout(()=> controller.abort(), 10000)
 
-
-    const initialTask: ResponseTask[] = await fetch("http://localhost:8080/api/getTask", {
+ 
+    const initialTask:ResponseTask[] = await fetch("http://localhost:8080/api/getTask", {
         headers: {
             'Content-Type': 'application/json',
         },
@@ -27,14 +28,14 @@ export default async function TaskPage({searchParams}: PageByProps) {
         if (!text) return [];
         return JSON.parse(text);
     }).catch((error) => {
-        if (error.name === 'AbortError') {
-            console.log('Fetch 요청이 시간초과되었습니다.')
-        }
-        console.error('Error:', error)
+            if(error.name=== 'AbortError'){
+                console.log('Fetch 요청이 시간초과되었습니다.')
+            }
+            console.error('Error:', error)
     }).finally(() => clearTimeout(timeoutId));
 
 
-    const customerAffiliations: CustomerAffiliation[] = await fetch("http://localhost:8080/api/getAffiliation", {
+    const customerAffiliations:CustomerAffiliation[] = await fetch("http://localhost:8080/api/getAffiliation",{
         next: {revalidate: 3600000, tags: ['affiliation']} //1시간마다 재검증
     }).then(async (response) => {
         if (!response.ok) {
@@ -43,14 +44,35 @@ export default async function TaskPage({searchParams}: PageByProps) {
         const text = await response.text();
         if (!text) return [];
         return JSON.parse(text);
+    }).catch((error) => {console.error('Error:', error)})
+
+    const employees: ResponseEmployee[] = await fetch("http://localhost:8080/api/getEmployees", {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        // cache:'no-store',
+        next: {revalidate: 3600000, tags: ['employee']} //1시간마다 재검증
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        const text = await response.text();
+        if (!text) return [];
+        return JSON.parse(text);
     }).catch((error) => {
+        if (error.name === 'AbortError') {
+            console.log('Fetch 요청이 시간초과되었습니다.')
+        }
         console.error('Error:', error)
     })
-
-
-    return (
-        <section>
-            <TaskSearch customerAffiliations={customerAffiliations} initialTask={initialTask} page={page}/>
+    
+    return(
+        <section key={JSON.stringify(initialTask)+page}>
+           <TaskSearch
+                customerAffiliations={customerAffiliations}
+                initialTask={initialTask}
+                employees={employees}
+                page={page}/>
         </section>
     )
 }
