aea42b1bc3b1c53f00a9bed10707fe4409c2273c
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-11 18:47:37 +0900

    feat: Before rebase commit

--- src/main/front/features/task/task/action/taskSearchAction.tsx
+++ src/main/front/features/task/task/action/taskSearchAction.tsx
@@ -6,7 +6,7 @@ import {v4 as uuidv4} from "uuid";
 export const initialTaskState = {
     customer:'',
     taskType: 'none', //구분
-    customerAffiliation: 'none', //거래처구분
+    affiliation: 'none', //거래처구분
     assignedUser: 'none', //담당자
     task:[],
     searchKey:uuidv4(),
@@ -16,22 +16,22 @@ export async function taskSearchAction(prevState, formData){
     const searchData:TaskSearchCondition = {
         customer: formData.get('customer'),
         taskType: formData.get('taskType'), //구분
-        customerAffiliation: formData.get('customerAffiliation'), //거래처구분
+        affiliation: formData.get('affiliation'), //거래처구분
         assignedUser: formData.get('assignedUser'), //담당자
     }
     if(searchData.taskType==='none'){
         searchData.taskType = null;
     }
-    if(searchData.customerAffiliation==='none'){
-        searchData.customerAffiliation =null;
+    if(searchData.affiliation==='none'){
+        searchData.affiliation =null;
     }
     if(searchData.assignedUser==='none'){
         searchData.assignedUser = null;
     }
     const searchKey = uuidv4()
 
-    const{taskType, customer, assignedUser,customerAffiliation} = searchData
-    if(!taskType && !customer && !assignedUser && !customerAffiliation){
+    const{taskType, customer, assignedUser, affiliation} = searchData
+    if(!taskType && !customer && !assignedUser && !affiliation){
         return {...prevState,...searchData, searchKey}
     }
     const task = await fetchSearchTask(searchData)
