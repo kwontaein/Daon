9b414ec2cf502f4c34f1269232025cd30e811986
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-13 15:49:36 +0900

    refactor: Change cache action

--- src/main/front/app/(search)/search-stock-items/page.tsx
+++ src/main/front/app/(search)/search-stock-items/page.tsx
@@ -2,9 +2,10 @@ import StockSearchItems from "@/components/share/search-items/stock-search";
 import { searchStockApi } from "@/features/stock/stock/api/searchStockApi";
 import { SearchNameProps } from "@/model/types/share/type";
 import { ResponseStock, StockSearchCondition } from "@/model/types/stock/stock/types";
+import { revalidateTag } from "next/cache";
 
 
-export const revalidate = 5 * 60 * 1; 
+export const revalidate = 10 * 60 * 1; 
 export default async function SearchCustomerItemsPage({searchParams} : SearchNameProps) {
     const page = (await searchParams).page ||1;
     const stockName = (await searchParams).searchName;
@@ -20,7 +21,11 @@ export default async function SearchCustomerItemsPage({searchParams} : SearchNam
     const searchResult:ResponseStock[] = await searchStockApi(postSearchInfo)
     const pageByStocks = searchResult.slice((page-1)*20, ((page-1)*20)+20)
 
-   
+    if (typeof window !== "undefined") {
+        window.addEventListener("beforeunload", () => {
+          revalidateTag(`${stockName}`);
+        });
+      }
     return(
         <StockSearchItems
           stocks={pageByStocks}
