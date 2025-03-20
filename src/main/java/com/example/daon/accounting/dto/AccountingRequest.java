3ae41115527bbd3659198c47ad114a6f9f4bb88b
kwontaein <zodls1128@gmail.com>
2025-03-20 11:24:47 +0900

    refactor : stock service 오류 수정

---
+++ src/main/java/com/example/daon/accounting/dto/AccountingRequest.java
@@ -0,0 +1,23 @@
+package com.example.daon.accounting.dto;
+
+public class AccountingRequest {
+    //분류선택
+    //날짜
+    //업체명
+    //사업자번호
+    //결제내역
+    //금액
+    //부가세
+    //합계
+    //비고
+    //메모
+
+    //모델명
+    //매입처
+    //수량
+    //인수
+    //설치
+    //결재
+
+    //카드사
+}
