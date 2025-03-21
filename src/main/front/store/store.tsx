9e65b47e66d7250752110ea63dcda9e64c7b61dc
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-21 14:31:51 +0900

    chore: Add stomp reducer

--- src/main/front/store/store.tsx
+++ src/main/front/store/store.tsx
@@ -1,16 +1,44 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
+
 import receiptSearchSlice from './slice/receipt-search'
 import receiptSelectorSlice  from './slice/receipt-select';
+import { initializeStompChannel } from './middleware/stomp-middleware';
+import { all } from "@redux-saga/core/effects"; 
+import StompReducer, { updateStompState } from './slice/stomp-reducer';
+import createSagaMiddleware from 'redux-saga';
 
+
+function* rootSaga() {
+  // all 함수는 여러 사가를 합쳐주는 역할을 한다.
+  yield all([initializeStompChannel()]);
+}
 
 const rootReducer = combineReducers({
+    stomp: StompReducer,
     receiptSearch: receiptSearchSlice,
     receiptSelector: receiptSelectorSlice,
 });
 
+const sagaMiddleware = createSagaMiddleware();
+
+
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(sagaMiddleware),
 });
+
+
+
+export const sagaTask =sagaMiddleware.run(rootSaga);
+export const sagaRefresh = ()=>{
+  //stomp연결해제
+  store.dispatch(updateStompState({isConnect:false}))
+
+  setTimeout(()=>{
+    sagaTask.cancel()
+    sagaTask;//취소 후 재연결
+  },50)
+}
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
