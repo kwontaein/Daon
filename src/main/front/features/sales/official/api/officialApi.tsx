f5f5e2b433e30a02f4b0f4e1c2ce2e4439b2480b
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-24 23:49:21 +0900

    Chore: Create official api

--- src/main/front/features/sales/official/api/officialApi.tsx
+++ src/main/front/features/sales/official/api/officialApi.tsx
@@ -1,75 +1,74 @@
-// 'use server';
+'use server';
 
-// import { StockCate } from "@/model/types/stock/cate/type";
-// import { revalidatePath, revalidateTag } from "next/cache";
-
-
-// export const updateStockCateApi = async (cates: StockCate[]) => {
-//     return fetch("http://localhost:8080/api/updateStockCate", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(cates),
-//         cache:'no-store'
-//     }).then(async (response) => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         if(response.status===500){
-//             window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
-//         }
-//         revalidateTag("stocksCate");
-//         // revalidatePath("/main/stock/stock-cate");
-//         return response.status
-//     }).catch((error) => {
-//         console.error('Error:', error)
-//     })
-// }
-
-// export const saveStockCateApi = async (stock: Pick<StockCate, 'stockCateName'>) => {
-//     return fetch("http://localhost:8080/api/saveStockCate", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(stock),
-//         cache:'no-store'
-//     }).then(async (response) => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         if(response.status===500){
-//             window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
-//         }
-//         revalidateTag("stocksCate");
-//         // revalidatePath("/main/stock/stock-cate");
-//         return response.status
-//     }).catch((error) => {
-//         console.error('Error:', error)
-//     })
-// }
+import { Official } from "@/model/types/receipt/type";
+import { revalidateTag } from "next/cache";
 
 
-// export const deleteStockCateApi =async (stock: StockCate) => {
-//     return fetch("http://localhost:8080/api/deleteStockCate", {
-//         method: "POST",
-//         headers: {
-//             'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(stock),
-//         cache:'no-store'
-//     }).then(async (response) => {
-//         if (!response.ok) {
-//             throw new Error(`HTTP error! status: ${response.status}`);
-//         }
-//         if(response.status===500){
-//             window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
-//         }
-//         revalidateTag("stocksCate")
-//         // revalidatePath("/main/stock/stock-cate");
-//         return response.status
-//     }).catch((error) => {
-//         console.error('Error:', error)
-//     })
-// }
+export const updateOfficialApi = async (official: Official[]) => {
+    return fetch("http://localhost:8080/api/updateOfficial", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(official),
+        cache:'no-store'
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        if(response.status===500){
+            window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
+        }
+        revalidateTag("official");
+        return response.status
+    }).catch((error) => {
+        console.error('Error:', error)
+    })
+}
+
+export const saveOfficialApi = async (official: Official[]) => {
+    return fetch("http://localhost:8080/api/saveOfficial", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(official),
+        cache:'no-store'
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        if(response.status===500){
+            window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
+        }
+        revalidateTag("officialsCate");
+        // revalidatePath("/main/official/official-cate");
+        return response.status
+    }).catch((error) => {
+        console.error('Error:', error)
+    })
+}
+
+
+export const deleteOfficialApi =async (official: Official) => {
+    return fetch("http://localhost:8080/api/deleteOfficial", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(official),
+        cache:'no-store'
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        if(response.status===500){
+            window.alert('문제가 발생했습니다 관리자에게 문의해주세요.')
+        }
+        revalidateTag("officialsCate")
+        // revalidatePath("/main/official/official-cate");
+        return response.status
+    }).catch((error) => {
+        console.error('Error:', error)
+    })
+}
