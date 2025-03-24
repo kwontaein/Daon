caffefc8bced3925d00c10ddb5a3b64bc4d16eab
kwontaein <zodls1128@gmail.com>
2025-03-24 23:16:08 +0900

    feat : 부가세 엔티티 생성

--- src/main/java/com/example/daon/accounting/model/SalesVATEntity.java
+++ src/main/java/com/example/daon/accounting/model/SalesVATEntity.java
@@ -1,11 +1,14 @@
 package com.example.daon.accounting.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -18,35 +21,48 @@ import java.util.UUID;
 @AllArgsConstructor
 public class SalesVATEntity {
     @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(nullable = false, unique = true, name = "sales_vat_id", columnDefinition = "BINARY(16)")
     private UUID salesVATId;
 
     // 분류선택
+    @Column(name = "category_selection")
     private String categorySelection;
 
     // 날짜
+    @Column(name = "date")
     private LocalDate date;
 
     // 업체명
+    @Column(name = "category_name")
     private String companyName;
 
     // 사업자번호
+    @Column(name = "business_number")
     private String businessNumber;
 
     // 결제내역
+    @Column(name = "payment_details")
     private String paymentDetails;
 
     // 금액
+    @Column(name = "amount")
     private BigDecimal amount;
 
     // 부가세
+    @Column(name = "vat")
     private BigDecimal vat;
 
     // 합계
+    @Column(name = "total")
     private BigDecimal total;
 
     // 메모
+    @Column(name = "memo")
     private String memo;
 
     //입금전환여부
+    @Column(name = "paid")
     private boolean paid;
 }
