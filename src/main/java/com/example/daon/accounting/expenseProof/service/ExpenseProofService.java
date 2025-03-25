2038719f61fab833aee91f3420cf97b0b9609a3a
kwontaein <zodls1128@gmail.com>
2025-03-25 18:06:18 +0900

    refactor : file structure rebuild

---
+++ src/main/java/com/example/daon/accounting/expenseProof/service/ExpenseProofService.java
@@ -0,0 +1,30 @@
+package com.example.daon.accounting.expenseProof.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ExpenseProofService {
+
+    //지출증빙
+    public void saveExpenseProof() {
+
+    }
+
+    public void updateExpenseProof() {
+
+    }
+
+    public void deleteExpenseProof() {
+
+    }
+
+    public void getExpenseProof() {
+
+    }
+
+    public void paidExpenseProof() {
+
+    }
+}
