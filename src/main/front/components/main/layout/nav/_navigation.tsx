5e7c5116f436e76b8457830c9612834b114d3ecd
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-26 17:42:26 +0900

    refactor Change global fontSize

--- src/main/front/components/main/layout/nav/_navigation.tsx
+++ src/main/front/components/main/layout/nav/_navigation.tsx
@@ -1,48 +1,20 @@
 import Link from 'next/link';
 import './_navigation.scss';
+import { AsideOptions } from '@/model/constants/asideOptions';
 
 export default function Navigation(){
-
+    const navigation_route = Object.entries(AsideOptions)
     return(
             <nav className={'nav-container'}>
                 <ul className={'nav-container__ul'}>
-                    <li>
-                        <b>|</b>
-                        <Link href={'/main/sales/receipt'}>판매회계</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/customer/customer'}>고객관리</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/stock/stock'}>품목/재고</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/ledger/ledger-customer'}>원장출력</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/staff/company'}>회사/사원관리</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/task/task'}>업무관리</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/accounting/pvat'}>회계 및 거래관리</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/board/board'}>사내게시판</Link>
-                        <b>|</b>
-                    </li>
-                    <li>
-                        <Link href={'/main/schedule/schedule'}>내일정관리</Link>
-                        <b>|</b>
-                    </li>
+                    
+                    {navigation_route.map(([currentNav, {asideTitle,asideItems}],index)=>(
+                        <li key={currentNav}>
+                            {index===0 && <b>|</b>}
+                            <Link href={`/main/${currentNav}/${asideItems[0].link}`}>{asideTitle}</Link>
+                            <b>|</b>
+                        </li>
+                    ))}
                 </ul>
             </nav>
     )
