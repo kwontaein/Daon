efc3e6dc0eb2e7e8205f9719e493c14dc9c84457
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-26 23:56:48 +0900

    chore: Add getTaskApi

--- src/main/front/app/(detail)/estimate/page.tsx
+++ src/main/front/app/(detail)/estimate/page.tsx
@@ -6,20 +6,18 @@ import { EstimateRegisterProps, ResponseEstimate } from "@/model/types/sales/est
 import { ResponseCompany } from "@/model/types/staff/company/type";
 import { ResponseTask } from "@/model/types/sales/task/type";
 import getCompany from "@/features/staff/company/api/company-api";
-import getTask from "@/features/sales/task/api/taskApi";
+import {getTask} from "@/features/sales/task/api/taskApi";
 import { getEstimateApi } from "@/features/sales/estimate/api/estimateApi";
 import RegisterEstimate from "@/components/main/sales/estimate/form/register-estimate";
 
 export default async function EstimateDetailPage({searchParams}:EstimateRegisterProps){
-    const targetId = (await searchParams).taskId
+    const taskId = (await searchParams).taskId
     const estimateId = (await searchParams).target ||''
     const mode = (await searchParams).mode || 'detail'
 
     let estimate:ResponseEstimate = await getEstimateApi(estimateId);
     const companyList:ResponseCompany[] = await getCompany()
-    const taskList:ResponseTask[] =await getTask()
-
-    const task = taskList.find(({taskId})=>taskId ===targetId)
+    const task:ResponseTask =await getTask(taskId)
 
     return (
         <RegisterEstimate
