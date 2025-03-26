db9ace2fd8b8bd61b992a27bd14e0a4e0d472708
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-03-26 11:12:22 +0900

    feat: Fixed updateEstimateApi connect -  Re-connect updateEstimateApi - Add category no items view

--- src/main/front/app/main/[nav]/(stock)/point/page.tsx
+++ src/main/front/app/main/[nav]/(stock)/point/page.tsx
@@ -1,5 +1,4 @@
 import StockPoint from "@/components/main/stock/point/point";
-import '@/components/main/stock/point/point.scss';
 export default async function StockPointPage(){
     const InitStockPoint = await fetch("http://localhost:8080/api/getStockPointList",
         {
@@ -12,7 +11,7 @@ export default async function StockPointPage(){
 
     return (
         <section key={JSON.stringify(InitStockPoint)}>
-            <p className="stock-point-info">※ 입력하신 적립율을 품목소비가에 적용하게 됩니다.</p>
+            <p style={{color:'red', fontSize:'13px', marginBottom:'5px'}}>※ 입력하신 적립율을 품목소비가에 적용하게 됩니다.</p>
             <StockPoint InitStockPoint={[]}/>
         </section>
     )
