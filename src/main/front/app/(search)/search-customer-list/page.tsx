6f27cd28ed1e9d7db47498d814576eb0ad02368b
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-01 11:12:34 +0900

    chore: Create ledger actions & api -create ledger search ui

--- src/main/front/app/(search)/search-customer-list/page.tsx
+++ src/main/front/app/(search)/search-customer-list/page.tsx
@@ -1,10 +1,11 @@
+import CustomerListSearch from "@/components/share/search-items/customer-list-search";
 import CustomerSearchItems from "@/components/share/search-items/customer-search";
 import { searchCustomersApi } from "@/features/customer/customer/api/searchCustomerApi";
 import { CustomerSearchCondition, ResponseCustomer } from "@/model/types/customer/customer/type";
 import { SearchNameProps } from "@/model/types/share/type";
 import { revalidateTag } from "next/cache";
 
-export default async function SearchCustomerItemsPage({searchParams} : SearchNameProps) {
+export default async function SearchCustomerListPage({searchParams} : SearchNameProps) {
     const page = (await searchParams).page ||1;
     const customerName = (await searchParams).searchName;
 
@@ -16,7 +17,6 @@ export default async function SearchCustomerItemsPage({searchParams} : SearchNam
         ceo:null,
     }
     const searchResult:ResponseCustomer[] = await searchCustomersApi(postSearchInfo)
-    const pageByCustomers = searchResult.slice((page-1)*20, ((page-1)*20)+20)
 
     if (typeof window !== "undefined") {
         window.addEventListener("beforeunload", () => {
@@ -25,6 +25,6 @@ export default async function SearchCustomerItemsPage({searchParams} : SearchNam
       }
       
     return(
-        <CustomerSearchItems customers={pageByCustomers} page={page} pageLength={searchResult.length}/>
+        <CustomerListSearch initialcustomers={searchResult} page={page}/>
     )
 }
---
+++ src/main/front/features/ledger/api/ledgerApi.tsx
@@ -0,0 +1,55 @@
+import { LedgerSearchCondition } from "@/model/types/ledger/type";
+
+export async function getLedgerCustomerApi(searchCondition:LedgerSearchCondition){
+    console.log(searchCondition)
+    try {
+        const response = await fetch("http://localhost:8080/api/getLedgers", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(searchCondition),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const text = await response.text();
+        return text ? JSON.parse(text) : [];
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+export async function getLedgerCustomesrApi(searchCondition:LedgerSearchCondition){
+    try {
+        const response = await fetch("http://localhost:8080/api/getLedgers", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(searchCondition),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const text = await response.text();
+        return text ? JSON.parse(text) : [];
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
+
+
+
+export async function getLedgerStockApi(searchCondition:LedgerSearchCondition){
+    try {
+        const response = await fetch("http://localhost:8080/api/getStockLedger", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(searchCondition),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const text = await response.text();
+        return text ? JSON.parse(text) : [];
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
