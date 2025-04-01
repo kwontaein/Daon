70c250d3da8e30bb32670e46066057a51e9d900e
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-01 15:46:45 +0900

    chore: Connect delete receipt api

--- src/main/front/features/sales/receipt/api/receiptApi.tsx
+++ src/main/front/features/sales/receipt/api/receiptApi.tsx
@@ -28,6 +28,35 @@ export async function saveReceiptListApi(receiptList: ResponseReceipt[]) {
 }
 
 
+export async function deleteReceiptApi(ids: string[]) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+    return await fetch("http://localhost:8080/api/deleteReceipt", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ids}),
+        signal,
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.status
+
+    }).catch((error) => {
+        if (error.name === 'AbortError') {
+            console.log('Fetch 요청이 시간초과되었습니다.');
+        }
+        console.error('Error:', error);
+    }).finally(() => clearTimeout(timeoutId));
+}
+
+
+
+
 export async function getReceiptListApi(receiptCondition: ReceiptCondition) {
     const controller = new AbortController();
     const signal = controller.signal;
