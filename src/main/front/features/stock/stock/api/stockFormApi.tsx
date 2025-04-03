51f092d337a0c7856ee341d753a6b7f2be8d3549
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-03 15:42:14 +0900

    feat: Connect updateStockApi

--- src/main/front/features/stock/stock/api/stockFormApi.tsx
+++ src/main/front/features/stock/stock/api/stockFormApi.tsx
@@ -27,3 +27,31 @@ export async function saveStockApi(stock: Omit<ResponseStock,'stockId'>) {
         console.error('Error:', error);
     }).finally(() => clearTimeout(timeoutId));
 }
+
+export async function updateStockApi(stock:ResponseStock) {
+    const controller = new AbortController();
+    const signal = controller.signal;
+    const timeoutId = setTimeout(() => controller.abort(), 10000);
+
+    return await fetch("http://localhost:8080/api/updateStock", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(stock),
+        signal,
+
+    }).then(async (response) => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        return response.status;
+        
+    }).catch((error) => {
+        if (error.name === 'AbortError') {
+            console.log('Fetch 요청이 시간초과되었습니다.');
+        }
+        console.error('Error:', error);
+    }).finally(() => clearTimeout(timeoutId));
+}
