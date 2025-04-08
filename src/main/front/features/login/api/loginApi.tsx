a89496c537a373c50ec2bc8723bcce036d21d88c
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-08 16:22:16 +0900

    feat: Connect sign-in api

---
+++ src/main/front/features/login/api/loginApi.tsx
@@ -0,0 +1,43 @@
+export const loginApi = async(userInfo:{userId:string, password:string})=>{
+    try {
+        const response = await fetch("http://localhost:8080/api/signIn", {
+            method: "POST",
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(userInfo),
+        });
+
+        if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
+
+        const text = await response.text();
+        return text ? JSON.parse(text) : [];
+    } catch (error) {
+        useLoginFilter(error)
+        jwtFilter(error)
+    }
+}
+
+function useLoginFilter(error: string): void {
+    if (error === "NON_EXISTENT_ERROR") {
+        window.alert("존재하지 않는 아이디 입니다. 다시 확인해주세요.")
+
+    } else if (error === "PW_ERROR") {
+        window.alert("비밀번호가 틀렸습니다. 다시 입력해주세요.")
+    } else {
+        window.alert("알 수 없는 오류가 발생했습니다. 관리자에게 문의하세요.")
+    }
+}
+
+export function jwtFilter(error: string): void {
+    console.log(error)
+    if (error === "302") {
+        window.alert("모든 토큰이 만료되었습니다. 재로그인하세요.")
+        document.location.replace('/sign')
+
+    } else if (error === "401") {
+        window.alert("알수없는 접근입니다 재로그인하세요.")
+
+    } else if (error === "409") {
+        window.alert("다른 곳에서 로그인되었습니다. 로그아웃합니다")
+        document.location.replace('/sign')
+    }
+}
