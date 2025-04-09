728b741091e27c46b22c0df2446c742bb7236065
kwontaein <zodls1128@gmail.com>
2025-04-09 14:12:01 +0900

    refactor : GlobalService convertToLedgerResponse update

--- src/main/java/com/example/daon/ledger/dto/response/LedgerResponse.java
+++ src/main/java/com/example/daon/ledger/dto/response/LedgerResponse.java
@@ -1,7 +1,6 @@
 package com.example.daon.ledger.dto.response;
 
 import com.example.daon.customer.model.CustomerEntity;
-import com.example.daon.estimate.model.EstimateEntity;
 import com.example.daon.receipts.model.ReceiptCategory;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -17,21 +16,24 @@ import java.util.UUID;
 @AllArgsConstructor
 @Builder
 public class LedgerResponse {
+    //날짜 / 계정 / 품목 / 수량 / 단가 / 토탈 / enum
     private UUID receiptId; // 전표 아이디
 
-    private EstimateEntity estimate; // 견적서 아이디
+    private LocalDateTime timeStamp; // 날짜
 
-    private LocalDateTime timeStamp; // 전표 등록일
-
-    private ReceiptCategory category; // 전표 분류 (ENUM 사용)
+    private ReceiptCategory category; // 전표 분류 (ENUM 사용) (계정)
 
     private CustomerEntity customer; // 고객 아이디
 
-    private UUID itemNumber; // 품목 아이디
+    private String productName; // 품목 명
+
+    private BigDecimal outPrice; // 품목 가격
 
     private Integer quantity; // 사용 품목 수량
 
     private BigDecimal totalPrice; // 품목 총 가격
 
-    private String description; // 전표 설명 (적요)
+    private String memo; //메모
+
+    private String description; //적요
 }
