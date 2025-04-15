3b685780797cd5298c66cef0438ca158544889b8
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-15 21:43:36 +0900

    chore: Create customers ledger search result

---
+++ src/main/front/components/main/ledger/ledger-customers/search-result.tsx
@@ -0,0 +1,170 @@
+import '../ledger-search-result.scss';
+
+import { ResponseLedger } from "@/model/types/ledger/type";
+import { ReceiptCategoryEnum } from "@/model/types/sales/receipt/type";
+import dayjs from "dayjs";
+import React, { JSX, useMemo, useReducer } from "react";
+import { useScreenMode } from '@/hooks/share/useScreenMode';
+import { renderLedgerRow, renderMobileLedgerRow, renderMobileSubTotalRow, renderMobileTotalRow, renderSubTotalRow, renderTotalRow, SubTotal, Total, updateSubTotals } from './ledger-result-rows';
+type Accumulator = {
+  elements: JSX.Element[];
+  date: Date;
+  balance: number;
+  subTotalOfTheDay: SubTotal;
+  total: Total;
+  ledgerCount: number;
+};
+
+  
+const initialSubTotal: SubTotal = {
+  sales: 0,
+  purchase: 0,
+  slaes_discount: 0,
+  purchase_discount: 0
+};
+
+const initialTotal: Total = {
+  quntity: 0,
+  sales: 0,
+  purchase: 0,
+  slaes_discount: 0,
+  purchase_discount: 0
+};
+
+
+export default function LedgerCustomersSearchResult({searchInfo}:{searchInfo:{searchTitle:string,searchResult:ResponseLedger[],searchSDate:Date}}){
+  const screen = useScreenMode({tabletSize:800, mobileSize:620})
+  const [mobileView,setMobileView] = useReducer((prev)=>!prev,false)
+
+  const mode =( screen!=='pc' ? screen: (mobileView ? 'tabelt': screen) )
+  const resultReducer = useMemo(()=>
+  
+    searchInfo.searchResult.reduce<Accumulator>((prev, ledger, idx) => {
+      const category = ReceiptCategoryEnum[ledger.category];
+      const isSameDate = prev.date === ledger.timeStamp;
+      const balance =
+        ['매출', '매입할인','출금',' 반품출고'].includes(category)
+          ? prev.balance + ledger.totalPrice
+          : prev.balance - ledger.totalPrice;
+  
+      const { newSub, newTotal } = updateSubTotals(category, ledger.totalPrice, ledger.quantity, isSameDate ? prev.subTotalOfTheDay : initialSubTotal, prev.total);
+      const elements = [...prev.elements];
+  
+      elements.push( mode==='pc' ?renderLedgerRow(ledger, balance, isSameDate) : renderMobileLedgerRow(ledger, balance, isSameDate));
+  
+      let ledgerCount = prev.ledgerCount + 1;
+      const isLast = idx === searchInfo.searchResult.length - 1;
+      const isNewDate = ledger.timeStamp !== searchInfo.searchResult[idx + 1]?.timeStamp; 
+  
+      //다음 ledger이랑 날짜가 다르거나 없으면 당일소계를 추가
+      if ((ledgerCount > 1 && isNewDate) || isLast) {
+        elements.push( mode ==='pc' ? renderSubTotalRow(idx, newSub) : renderMobileSubTotalRow(idx, newSub));
+        ledgerCount = 0;
+      }
+      //마지막이면 total추가
+      if (isLast) {
+        elements.push(mode==='pc' ? renderTotalRow(idx, newTotal, balance): renderMobileTotalRow(idx, newTotal,balance));
+      }
+  
+      return {
+        elements,
+        date: ledger.timeStamp,
+        balance,
+        subTotalOfTheDay: newSub,
+        total: newTotal,
+        ledgerCount
+      };
+    }, {
+      elements: [],
+      date: searchInfo.searchSDate,
+      balance: 0,
+      subTotalOfTheDay: initialSubTotal,
+      total: initialTotal,
+      ledgerCount: 0
+    })  ,[mode, searchInfo.searchResult])
+    
+
+    return(
+        <>
+        {screen ==='pc'&&
+          <label className='mobile-view-label'>
+            <input type='checkbox' checked={mobileView} onChange={setMobileView}/> 모바일 원장
+        </label>
+        }
+        <h3 className='ledger-title'>{searchInfo.searchTitle+' 원장'}</h3>
+        <div className='ledger-date-container'>{`Date : ${dayjs(new Date()).format('YYYY.MM.DD')}, Tel: ,Fax:`}</div>
+        <table className={`ledger-search-result-table ${ mode!=='pc' && ' ledger-mobile'}`} style={{marginTop:'0'}}>
+            <colgroup>
+                {mode==='pc'?
+                  <>
+                  <col style={{width: "7%"}}/>    
+                  <col style={{width: "6%",minWidth:'35px'}}/>
+                  <col style={{width: "15%"}}/>       
+                  <col style={{width: "10%", minWidth:'80px'}} />   
+                  <col style={{width: "5%", minWidth:'35px'}}/>    
+                  <col style={{width: "7%"}}/>    
+                  <col style={{width: "7%", minWidth:'65px'}}/>    
+                  <col style={{width: "7%", minWidth:'65px'}}/>   
+                  <col style={{width: "6%", minWidth:'60px'}}/>    
+                  <col style={{width: "6%", minWidth:'60px'}}/>    
+                  <col style={{width: "8%", minWidth:'60px'}}/>    
+                  <col style={{width: "10%"}}/>   
+                </>
+                :
+                <>
+                  <col style={{width: "7%"}}/>    
+                  <col style={{width: "6%",minWidth:'36px'}}/> 
+                  <col style={{width: "15%"}}/>          
+                  <col style={{width: "10%", minWidth:'85px'}} />   
+                  <col style={{width: "5%"}}/>    
+                  <col style={{width: "7%", minWidth:'80px'}}/>    
+                  <col style={{width: "7%", minWidth:'80px'}}/>    
+                  <col style={{width: "7%", minWidth:'60px'}}/>    
+                  <col style={{width: "10%"}}/>   
+                </>  
+              }
+            </colgroup>
+            <thead>
+                <tr>
+                    <td rowSpan={mode==='pc'? 1 :2}>날짜</td>
+                    <td rowSpan={mode==='pc'? 1 :2}>계정</td>
+                    <td rowSpan={mode==='pc'? 1 :2}>상호</td>
+                    <td rowSpan={mode==='pc'? 1 :2}>품목</td>
+                    <td>수량</td>
+                    {mode==='pc' &&<td>단가</td>}
+                    <td>판매/출금</td>
+                    <td>구매/입금</td>
+                    {mode==='pc' &&<td>매출할인</td>}
+                    {mode==='pc' &&<td>매입할인</td>}
+                    <td rowSpan={mode==='pc'? 1 :2}>잔액</td>
+                    <td rowSpan={mode==='pc'? 1 :2}>비고</td>
+                </tr>
+                {mode!=='pc' &&
+                  <tr>
+                    <td>단가</td>
+                    <td>매입할인</td>
+                    <td>매출할인</td>
+                  </tr>
+                }
+            </thead>
+            <tbody>
+                <tr style={{height:'20px'}}>
+                    <td>{dayjs(searchInfo.searchSDate).format('YY.MM.DD')}</td>
+                    <td></td>
+                    <td className="left-align" style={{fontSize:'0.95rem'}}>{`<전 기 이 월>`}</td>
+                    <td></td>
+                    <td></td>
+                    <td></td>
+                    {mode==='pc' && <td></td>}
+                    {mode==='pc' && <td></td>}
+                    {mode==='pc' && <td></td>}
+                    <td className="right-align">0</td>
+                    <td></td>
+                </tr>
+                {mode ==='pc' && resultReducer.elements}
+            </tbody>
+                {mode !=='pc' && resultReducer.elements}
+        </table>
+        </>
+        )
+}
--- src/main/front/components/main/ledger/ledger-customers/search.tsx
+++ src/main/front/components/main/ledger/ledger-customers/search.tsx
@@ -4,25 +4,44 @@ import '@/styles/table-style/search.scss';
 
 import CustomDateInput from '@/components/share/custom-date-input/custom-date-input';
 import { ResponseCustomer } from '@/model/types/customer/customer/type';
-import { startTransition, useActionState, useCallback, useEffect, useRef } from 'react';
+import { startTransition, useActionState, useCallback, useEffect, useMemo, useRef, useState } from 'react';
 import { initialLedgertState, ledgerSearchAction } from '@/features/ledger/actions/ledgerSearchAction';
 import useSearchCustomerList from '@/hooks/customer/search/useSearchCustomerList';
+import LedgerCustomersSearchResult from './search-result';
 
 export default function LedgerCustomerListSearch(){
     const [state, action] = useActionState(ledgerSearchAction,initialLedgertState)
     const formRef = useRef(null)
-        //거래처 검색관련
 
-    const checkCustomerId = useCallback(() => !!state.customerIds, [state.customerIds]);
+    const [searchInfo, setSearchInfo] = useState({
+        searchResult:[],
+        searchTitle:null,
+        searchSDate:null
+    })
+    useEffect(()=>{
+        if(state.searchResult){
+            console.log(state.searchResult)
+            setSearchInfo({
+                searchResult:state.searchResult,
+                searchTitle:`${state.searchSDate} ~ ${state.searchEDate} 복수거래처원장`,
+                searchSDate: state.searchSDate
+            })
+        }
+    },[state])
+
+    
+    //거래처 검색관련    
+    const checkCustomerId = useCallback(() => {
+        return (state.customerIds??[]).length >0
+    }, [state.customerIds]);
 
     const changeHandler = useCallback(<T extends Record<string, string>[]>(customerList: T) => {
         if (formRef.current) {
             const formData = new FormData(formRef.current);
             const customerIds = customerList.map(({customerId})=>customerId)
-            const customerName = customerList.map(({customerName})=>customerName)
-            console.log(customerIds.toString(),customerName.toString())
+            const customerNames = customerList.map(({customerName},idx)=> (idx!==0 ? ' ' : '') + customerName)
             formData.set('customerIds' ,JSON.stringify(customerIds));
-            formData.set('customerName' ,customerName.toString());
+            formData.set('customerName' ,customerNames.toString());
             
 
             startTransition(() => {
@@ -38,7 +57,7 @@ export default function LedgerCustomerListSearch(){
     const searchCustomerHandler = useSearchCustomerList(checkCustomerId, changeCustomerHandler);
 
     const submitHandler =() => {
-        if(!state.customerIds){
+        if((state.customerIds??[]).length===0){
             window.alert('거래처를 선택해주세요')
             return
         }
@@ -51,6 +70,7 @@ export default function LedgerCustomerListSearch(){
     
 
     return(
+        <>
         <section className='search-container'>
             <form action={action} ref={formRef}>
             <table className='search-table'>
@@ -82,7 +102,7 @@ export default function LedgerCustomerListSearch(){
                                    defaultValue={state.customerName} 
                                    key={state.customerName}
                                    onKeyDown={searchCustomerHandler}/>
-                            <input type='hidden' name='customerIds' value={JSON.stringify(state.customerIds)} readOnly/>
+                            <input type='hidden' name='customerIds' value={JSON.stringify(state.customerIds)+''} readOnly/>
                         </td>              
                     </tr>
                     <tr>
@@ -114,5 +134,9 @@ export default function LedgerCustomerListSearch(){
             </table>
             </form>
         </section>
+        {searchInfo.searchResult.length>0 &&
+            <LedgerCustomersSearchResult searchInfo={searchInfo}/>
+        }
+        </>
     )
 }
