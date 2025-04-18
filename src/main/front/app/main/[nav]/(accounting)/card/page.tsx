741e3a62adbaf21b1218a9b5e3fa0e1661db7fa8
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-18 10:01:05 +0900

    feat: Create getAccounting api

--- src/main/front/app/main/[nav]/(accounting)/card/page.tsx
+++ src/main/front/app/main/[nav]/(accounting)/card/page.tsx
@@ -1,10 +1,11 @@
 import AccountingSearch from "@/components/main/accounting/search";
+import { getCardTransactionfApi } from "@/features/accounting/api/accountingSearchApi";
 import getCompany from "@/features/staff/company/api/company-api";
 
 export default async function CardPage(){
     
     const companyList = await getCompany()
-    
+    const cardTransactionList = await getCardTransactionfApi()
     return(
         <AccountingSearch companyList={companyList} division='card'/>
     )
