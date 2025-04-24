6daaa0403c6e26a13c2fbe2edccd8cdf4d3de1b2
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-24 19:07:08 +0900

    feat: Change global state tool redux => zustand

--- src/main/front/app/main/[nav]/(sales)/receipt/@summary/(.)daily-summary/page.tsx
+++ src/main/front/app/main/[nav]/(sales)/receipt/@summary/(.)daily-summary/page.tsx
@@ -3,28 +3,24 @@ import Calendar from "@/components/main/sales/receipt/aside/calendar";
 import DateSummary from "@/components/main/sales/receipt/aside/date-summary";
 import { getRecieptTotalApi } from "@/features/sales/receipt/api/receiptApi";
 import { ResponseReceiptTotal } from "@/model/types/sales/receipt/type";
-import { updateSearchDateId } from "@/store/slice/receipt-search";
 
-import { RootState } from "@/store/store";
+import { useDailySummary } from "@/store/zustand/receipt-search";
 import '@/styles/_global.scss'
 import '@/styles/main-view/daily-summary.scss'
 import { useRouter } from "next/navigation";
 import { useCallback, useState } from "react";
-import { useDispatch } from "react-redux";
-import { useSelector } from "react-redux";
+
 
 export default function DailySummaryPage(){
     const router = useRouter();
 
-    const {date} = useSelector((state:RootState)=> state.receiptSearch)
+    const {date, updateSearchDateId} = useDailySummary()
     const [receiptTotal, setReceiptTotal] =useState<ResponseReceiptTotal>()
-
-    const dispatch = useDispatch()
 
     const searchSummary = useCallback(()=>{
         getRecieptTotalApi(date).then((res)=>{
             setReceiptTotal(res)
-            dispatch(updateSearchDateId())
+            updateSearchDateId()
         })
     },[date])
 
