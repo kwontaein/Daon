8d467c059a0c486e3e6e8101ac682719673927e4
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-30 16:42:03 +0900

    chore: Create zustand file to userInfo & modalState

---
+++ src/main/front/store/zustand/userInfo.tsx
@@ -0,0 +1,23 @@
+import { EmployeeClassEnum, UserRoleEnum } from '@/model/types/staff/employee/type'
+import {create} from 'zustand'
+
+
+export type UserInfo ={
+    userId:string
+    userName:string
+    class: EmployeeClassEnum,
+    role: UserRoleEnum,
+    last_login:Date
+}
+
+export type UserState ={
+    user:Partial<UserInfo>,
+    setUser:(user:UserInfo)=>void
+    deleteUser:()=>void,
+}
+
+export const useUserInformation = create<UserState>(set=>({
+    user:{},
+    setUser:(user:UserInfo)=>set({user}),
+    deleteUser:()=>set({user:{}}),
+}))
