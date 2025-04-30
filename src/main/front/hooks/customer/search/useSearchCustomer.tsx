0860a5dd082a120dfb525da7021aa3fa3fac2deb
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-04-30 14:41:33 +0900

    fix: Fixed receipt customer input

--- src/main/front/hooks/customer/search/useSearchCustomer.tsx
+++ src/main/front/hooks/customer/search/useSearchCustomer.tsx
@@ -14,7 +14,11 @@ export default function useSearchCustomer(
             if (event.data) {
                 const { customerName, customerId} = event.data;
                 if(customerName && customerId){
-                    changeHandler({...event.data} , target)
+                    const data = Object.fromEntries(
+                        Object.entries(event.data).filter(([key])=> 
+                            ['customerName','customerId','businessNumber','phoneNumber','address1','address2','zipCode'].includes(key)
+                        ))
+                    changeHandler({...data} , target)
                 }
             }
         };
