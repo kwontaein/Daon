556309632e3876e35dd8fc822cd67fa9fc82f4c1
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-09 00:18:22 +0900

    feat: Complete Calendar v1

--- src/main/front/model/types/schedule/type.tsx
+++ src/main/front/model/types/schedule/type.tsx
@@ -1,7 +1,23 @@
-export type Schedule={
+export type RequestSchedule={
     calendarId?:string;
     date:Date;
     memo:string;
     userId:string;
-    month:string; 
+    month?:number; 
+    year?:number; 
+}
+
+export type ResponseSchedule={
+    calendarId?:string;
+    date:Date;
+    memo:string;
+    userId:string;
+    userName:string; 
+}
+
+export type ScheduleDateOfKey ={
+    [date:string]:{
+        memo:string,
+        userId:string,
+    }
 }
