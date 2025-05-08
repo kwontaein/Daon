fbe59bc8bfb22e6322087740e6e856d817ed4b16
kwontaein <zodls1128@gmail.com>
2025-05-08 17:19:01 +0900

    refactor : Config update

--- src/main/java/com/example/daon/config/WebMvcConfig.java
+++ src/main/java/com/example/daon/config/WebMvcConfig.java
@@ -13,10 +13,10 @@ public class WebMvcConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedOrigins("http://localhost:3000").allowedHeaders("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowCredentials(true)
+                        .maxAge(3600);
             }
         };
     }
