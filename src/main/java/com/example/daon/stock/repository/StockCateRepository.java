30d922af9d4a7e8e6926a4738a5addd7984173b8
kwontaein <zodls1128@gmail.com>
2025-05-10 14:20:28 +0900

    feat : Bbs api generate refactor : DIY add in repositories

--- src/main/java/com/example/daon/stock/repository/StockCateRepository.java
+++ src/main/java/com/example/daon/stock/repository/StockCateRepository.java
@@ -4,10 +4,12 @@ import com.example.daon.stock.model.StockCate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface StockCateRepository extends JpaRepository<StockCate, UUID>, JpaSpecificationExecutor<StockCate> {
     Optional<StockCate> findByStockCateName(@Param("name") String name);
 
