5cfa097fc140aa16d040535981e20c30304ea203
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-14 00:32:37 +0900

    feat: Create permssion employee Component

--- src/main/front/app/main/[nav]/(staff)/employee/page.tsx
+++ src/main/front/app/main/[nav]/(staff)/employee/page.tsx
@@ -1,3 +1,4 @@
+import PermissionManagementForm from "@/components/main/staff/employee/employee-permission-form";
 import EmployeeTable from "@/components/main/staff/employee/employee-table";
 import CustomLoading from "@/components/share/loading/loading";
 import { getEmployeeApi } from "@/features/staff/employee/api/employeeApi";
@@ -6,11 +7,26 @@ import { ResponseEmployee } from "@/model/types/staff/employee/type";
 import { Suspense } from "react";
 
 
-export default async function StaffPage({searchParams}:PageByProps){
+export default async function StaffPage({searchParams} : {
+    searchParams:Promise<{
+        page:number,
+        permissionTarget?:string
+    }>
+}) {
     const page = (await searchParams).page ||1
-    const controller = new AbortController();
+    const permissionTarget= (await searchParams).permissionTarget
 
     const initialEmployee:ResponseEmployee[] = await getEmployeeApi()
+
+    //permissionTarget이 존재하면 권한관리 창으로 전환
+    if(permissionTarget!==undefined){
+        const findEmployee = initialEmployee.find(({userId})=>userId===permissionTarget.trim())
+        const {userId, name} = findEmployee
+        if(userId && name){
+            return <PermissionManagementForm userId={userId} userName={name}/>
+        }
+    }
+    
     return(
         <section>
             <Suspense fallback={<CustomLoading content="직원 정보를 불러오고 있습니다."/>}>
