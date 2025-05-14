73142d401f6a3079a168bdbc0c87d71a334a4cc1
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-14 15:33:45 +0900

    feat: Make type Permission to AsideOptions

--- src/main/front/features/staff/employee/action/employee-permission-action.tsx
+++ src/main/front/features/staff/employee/action/employee-permission-action.tsx
@@ -1,9 +1,10 @@
 import { AsideOptions } from "@/model/constants/routes/asideOptions"
+import { ListOfAside } from "@/model/types/staff/employee/type";
 function kebabToCamel(str: string): string {
     return str.replace(/-([a-z])/g, (_, char) => char.toUpperCase());
   }
 export async function permissionFormAction(prevState,formState){
-    const permissionState ={
+    const permissionState:ListOfAside & { userId:string } ={
         ...(Object.fromEntries(Object.entries(AsideOptions).reduce((prev,[nav,{asideItems}])=>{
             const checkedByAsideState = formState.getAll(nav)
             asideItems.forEach(({link})=>{
--- src/main/front/features/staff/employee/api/employeeApi.tsx
+++ src/main/front/features/staff/employee/api/employeeApi.tsx
@@ -183,3 +183,31 @@ export const deleteEmployeeApi = async (userId: string) => {
         throw new Error('알 수 없는 오류가 발생했습니다.');
     })
 }
+
+export const getEnableUrl =async(userId)=>{
+    const accessToken = (await cookies()).get('accessToken')?.value
+    const cookie = `accessToken=${accessToken}`
+    try{
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getEnableUrl`, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json',
+                Cookie: cookie
+            },
+            credentials: 'include',
+            body: JSON.stringify({userId}),
+            cache: 'no-store'
+        })
+        await jwtFilter(response.status.toString());
+        const text = await response.text();
+
+        if (!text) return null;
+        return JSON.parse(text);
+    }catch (error) {
+        if (error instanceof Response) {
+            const { message } = await error.json();
+            throw new Error(message);
+        }
+        throw new Error('알 수 없는 오류가 발생했습니다.');
+    }
+}
