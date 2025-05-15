ff2048734c7d8ca88d21df5b8af63c28d911b43f
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-15 20:06:52 +0900

    feat: Check enable url from middleware

--- src/main/front/features/login/api/loginApi.tsx
+++ src/main/front/features/login/api/loginApi.tsx
@@ -1,3 +1,7 @@
+'use server'
+import jwtFilter from "@/features/share/jwtFilter";
+import {cookies} from "next/headers";
+
 export const loginApi = async (userInfo: { userId: string, password: string }) => {
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/signIn`, {
@@ -31,53 +35,15 @@ function loginFilter(error: string): void {
 
 
 
-export  function jwtFilter(statusCode: string): Promise<void> {
-    const redirectUrl = '/'; // 로그인 경로
-    const errorMessage = {
-        '401': '로그인이 필요합니다.',
-        '403': '접근 권한이 없습니다.',
-        '500': '서버 오류입니다. 관리자에게 문의하세요.',
-        '404': '데이터가 존재하지 않습니다.'
-      };
-
-    // 서버 환경: window가 undefined
-    if (typeof window === 'undefined') {
-      // 서버에서는 응답을 throw해서 API 핸들러가 적절하게 처리하게 함
-        if (["401", "403", "500"].includes(statusCode)) {
-            alert( errorMessage[statusCode])
-
-            throw new Response(JSON.stringify({ code: 'Unauthorized' ,message: errorMessage[statusCode] }), {
-                status: parseInt(statusCode, 10),
-                headers: {
-                'Content-Type': 'application/json',
-                },
-            });
-        }
-        return
-    }
-  
-    // 클라이언트 환경
-    switch (statusCode) {
-        case '401':
-        case '403':
-        alert(errorMessage[statusCode]);
-        window.location.replace(redirectUrl);
-        break;
-        case '500':
-        alert(errorMessage[statusCode]);
-        break;
-        default:
-        break;
-    }
-}
-
 export async function getUserInfo(){
-
+   const accessToken = (await cookies()).get('accessToken')?.value
+    const cookie = `accessToken=${accessToken}`
     try {
         const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/getMyDetail`, {
             credentials: "include", 
             headers : {
-                'Content-Type': 'application/json'
+                'Content-Type': 'application/json',
+                Cookie: cookie
             },
         })
         await jwtFilter(response.status.toString());
--- src/main/front/middleware.ts
+++ src/main/front/middleware.ts
@@ -3,28 +3,70 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { AUTH_ROUTES, MAIN_URL, PUBLIC_ROUTES } from './model/constants/routes/asideOptions';
 import { cookies } from 'next/headers';
+import { kebabToCamel } from './features/share/kebabToCamel';
+
 
 export async function middleware(request: NextRequest) {
     const response = NextResponse.next();
-    const { pathname } =request.nextUrl;
+    const { pathname, searchParams } =request.nextUrl;
 
-    const isPublicRoute = PUBLIC_ROUTES.includes(pathname)
     const cookie = (await cookies()).get('accessToken')?.value;
     const enable_url = (await cookies()).get('enable_url')?.value;
+    const isPublicRoute = PUBLIC_ROUTES.includes(pathname)
 
-    console.log(enable_url)
-    if(!cookie && pathname.startsWith('/main')){
-        console.log(isPublicRoute)
-        return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, request.url));
+    if(!enable_url){
+      response.cookies.delete('accessToken');
+      return response;
     }
-    if(cookie && isPublicRoute){
+    
+    if(!cookie && !pathname.startsWith('/')){
+        return NextResponse.redirect(new URL(AUTH_ROUTES.LOGIN, request.url));
+    }else if(cookie && isPublicRoute){
         return NextResponse.redirect(new URL(MAIN_URL, request.url));
     }
+    const able_url = JSON.parse(enable_url)
 
-    const res = NextResponse.next();
-    
+    if(pathname.startsWith(`/main`)){
+      const [main,,...route] = pathname.split('/')
+      if(route.length>1){
+        const [nav,aside,..._] = route
+        if(!able_url[kebabToCamel(nav)][kebabToCamel(aside)]){
+          // return NextResponse.redirect(new URL(MAIN_URL, request.url));
+        }
+      }
+    }else{
+      const mergeAside = Object.assign({},...Object.values(able_url)) //사용가능한 url 리스트
+      const division = searchParams.get('division')
+
+      // 검색은 예외 처리
+      if (pathname.includes('search')) return response;
+
+      const shouldBlock = () => {
+        // division 접근 제한 => accounting의 division에 따른 제한
+        if (division && !mergeAside[division]) return true;
+
+        // 업무처리창 접근 제한
+        if (pathname === 'action-taken' && !mergeAside['task']) return true;
+
+        // 전표 전환 접근 제한
+        if (pathname === 'trans-estimate' && !mergeAside['taskEstimate']) return true;
+
+        // 일반적인 register 페이지 접근 제한
+        const convertedKey = kebabToCamel(pathname.replace('register', ''));
+        if (mergeAside[convertedKey] === false) return true;
+
+        return false;
+      };
+
+      if (shouldBlock()) {
+        return new NextResponse(null, { status: 404 });
+      }
+    }
+
     return response;
-}
+  
+  }
+ 
 
 
 export const config = {
