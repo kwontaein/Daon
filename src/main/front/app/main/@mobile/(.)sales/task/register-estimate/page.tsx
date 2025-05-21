4c54f05376f09d8b9d699d843c378b362f42d5cf
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-21 17:47:06 +0900

    feat: Create estimate mobile view

---
+++ src/main/front/app/main/@mobile/(.)sales/task/register-estimate/page.tsx
@@ -0,0 +1,27 @@
+import EstimateHeader from "@/components/main/sales/estimate/form/estimate-header"
+import {getCompany} from "@/features/staff/company/api/company-api"
+import { getTaskApi } from "@/features/sales/task/api/taskApi"
+import { ResponseCompany } from "@/model/types/staff/company/type"
+import { EstimateRegisterProps } from "@/model/types/sales/estimate/type"
+import { ResponseTask } from "@/model/types/sales/task/type"
+import { getUserInfo } from "@/features/user/userApi"
+import MobileModal from "@/components/share/mobile-modal/page"
+
+export default async function RegisterEstimatePage({searchParams}:EstimateRegisterProps){
+    const taskId = (await searchParams).taskId ||''
+
+    const companyList:ResponseCompany[] = await getCompany()
+    const task:ResponseTask =await getTaskApi(taskId)
+    const userInfo = await getUserInfo()
+
+    return(
+        <MobileModal height="550px">
+            <EstimateHeader
+                companyList={companyList}
+                task={task}
+                mode='write'
+                userInfo={userInfo}
+                isMobile={true}/>
+        </MobileModal>
+    )
+}
