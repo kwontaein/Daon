af0243d192405c9c7240da6ad61ca22f5c52d3f3
kwontaein <zodls1128@gmail.com>
2025-05-26 21:14:22 +0900

    refactor : Get method block websocket

--- src/main/java/com/example/daon/global/service/GlobalService.java
+++ src/main/java/com/example/daon/global/service/GlobalService.java
@@ -71,7 +71,6 @@ public class GlobalService {
         SecurityContext securityContext = SecurityContextHolder.getContext();
         Authentication authentication = securityContext.getAuthentication();
         // Authentication 객체에서 유저 정보 추출
-        System.out.println("authentication.getPrincipal() : " + authentication.getPrincipal());
         return (UserDetails) authentication.getPrincipal(); //->principal ->id , password , 권한
         // 유저 정보 사용
     }
