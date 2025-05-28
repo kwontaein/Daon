7381047f2c9819f32fceb120515b8109952a8fcf
KXSXJ <100739107+KXSXJ@users.noreply.github.com>
2025-05-28 20:34:56 +0900

    feat: Enable trans-paid set trans date

--- src/main/front/components/main/accounting/trans-paid.tsx
+++ src/main/front/components/main/accounting/trans-paid.tsx
@@ -5,30 +5,27 @@ import Image from 'next/image'
 import asideArrow from '@/public/assets/aside-arrow.gif';
 import CustomDateInput from '@/components/share/custom-date-input/custom-date-input';
 import { startTransition, useActionState, useEffect, useRef } from 'react';
-import { transEstimateToReceiptApi } from '@/features/sales/estimate/api/server-api';
+import { AccountingDivision } from '@/model/types/accounting/type';
+import { transAccountingToReceipt } from '@/features/accounting/api/search-server-api';
 
-const transEstimateAction = async (prevState, formData)=>{
-    const formState = {
-        receiptDate:new Date(formData.get('receiptDate')),
-        note:formData.get('note'),
-        estimateId: formData.get('estimateId')
-    }
-    const status = await transEstimateToReceiptApi(formState) 
+const transPaidAction = async (prevState, formData)=>{
+    const id = formData.get('id')
+    const division = formData.get('division')
+    const paidDate = new Date(formData.get('paidDate'))
+    const status = await transAccountingToReceipt(division,id,paidDate) 
 
-    return {
-        ...formState,
-        status
-    }
+   return {...prevState, id, division, paidDate, status}
     
 }
 
-export default function TransEstimate({estimateId, isMobile}:{estimateId:string, isMobile?:boolean}){
+export default function TransPaid({division,id, isMobile}:{division:keyof typeof AccountingDivision,id:string, isMobile?:boolean}){
     const formRef = useRef(null)
-    const [state, action, isPending] = useActionState(transEstimateAction,{})
+    const [state, action, isPending] = useActionState(transPaidAction,{paidDate:new Date(Date.now())})
 
     const submitHandler =() => {
         const formData = new FormData(formRef.current);
-        formData.set('estimateId',estimateId)
+        formData.set('id',id)
+        formData.set('division',division)
         startTransition(() => {
             action(formData);
         });
@@ -37,8 +34,8 @@ export default function TransEstimate({estimateId, isMobile}:{estimateId:string,
     useEffect(()=>{
         if(state.status){
             if(state.status===200){
-                window.alert('전표전환이 완료되었습니다.')
-                isMobile ? window.history.back(): window.close()
+                window.alert('입금전환이 완료되었습니다.')
+                isMobile ? window.history.back() :window.close()
             }else{
                 window.alert('문제가 발생했습니다. 잠시 후 다시 시도해주세요.')
             }
@@ -49,27 +46,23 @@ export default function TransEstimate({estimateId, isMobile}:{estimateId:string,
         <section className='register-form-container' style={{padding:'8px', boxSizing:'border-box'}}>
             <header className="register-header">
                 <Image src={asideArrow} alt=">" width={15}/>
-                <h4>전표전환</h4>
+                <h4>입금전환</h4>
             </header>
             <form action={action} ref={formRef}>
                 <table className='register-form-table'>
                     <tbody>
                         <tr>
-                            <td className='table-label'>처리일자</td>
+                            <td className='table-label'>입금일</td>
                             <td>
-                                <CustomDateInput defaultValue={state.receiptDate} name='receiptDate'/>
+                                <CustomDateInput defaultValue={state.paidDate} name='paidDate'/>
                             </td>
-                        </tr>
-                        <tr>
-                            <td className='table-label'>비고</td>
-                            <td><textarea name='note' defaultValue={state.note}/></td>
                         </tr>
                     </tbody>
                 </table>
             </form>
             <div className='button-container'>
-                <button onClick={submitHandler} disabled={isPending}>전표전환</button>
-                <button onClick={()=>{isMobile? window.history.back():window.close()}}>취소</button>
+                <button type='button' onClick={submitHandler} disabled={isPending}>저&nbsp;&nbsp;&nbsp;&nbsp;장</button>
+                <button type='button' onClick={()=>{isMobile? window.history.back():window.close()}}>취&nbsp;&nbsp;&nbsp;&nbsp;소</button>
             </div>
        </section>
     )
