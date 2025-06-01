7ada453c5ed1a281bcbf7ff84f1d1cb908cec7ec
kwontaein <zodls1128@gmail.com>
2025-06-01 16:32:28 +0900

    refactor : allowedOrigin update

--- src/main/java/com/example/daon/config/WebMvcConfig.java
+++ src/main/java/com/example/daon/config/WebMvcConfig.java
@@ -13,7 +13,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000").allowedOrigins("http://admin.jacc.co.kr").allowedHeaders("*")
+                        .allowedOrigins("http://localhost:3000").allowedOrigins("https://daon-zeta.vercel.app/").allowedHeaders("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowCredentials(true)
                         .maxAge(3600);
