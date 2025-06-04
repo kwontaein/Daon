273fad90544eb13668e3e2ef2ecbc68aadc347e7
kwontaein <zodls1128@gmail.com>
2025-06-04 22:35:55 +0900

    refactor : cookie update domain setting

--- src/main/java/com/example/daon/admin/service/AdminService.java
+++ src/main/java/com/example/daon/admin/service/AdminService.java
@@ -120,7 +120,7 @@ public class AdminService {
                     .httpOnly(false)
                     .secure(true) // ✅ HTTPS 환경에서는 필수
                     .sameSite("None") // ✅ 도메인이 다를 경우 반드시 필요
-                    .path("/")
+                    .path("/").domain("daon-vta0.onrender.com")
                     .maxAge(Duration.ofHours(1))
                     .build();
             response.addHeader("Set-Cookie", cookie.toString());
@@ -161,7 +161,7 @@ public class AdminService {
                 .httpOnly(false)
                 .secure(true) // 배포 환경 기준
                 .sameSite("None") // 설정했으면 지울 때도 똑같이
-                .path("/")
+                .path("/").domain("daon-vta0.onrender.com")
                 .maxAge(0) // 즉시 만료
                 .build();
 
